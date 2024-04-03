026b20cd0f7a15dade159750ff148fc860de0d4d
fabian9799 <fabian9799@protonmail.ch>
2024-04-03 19:21:11 +0200

    fix: snippet cards

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,21 +6,21 @@
 
 <main class="p-8 max-w-2xl mx-auto">
 	<p class="text-3xl font-bold mb-4">Snippets</p>
-	<ul class=" grid grid-cols-2 flex-wrap gap-2">
+	<ul class="grid grid-cols-2 flex-wrap gap-2">
 		{#each data.snippets as { slug, description, title, tags }}
-			<li
-				class="border border-zinc-800 p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/10 hover:border-rose-800"
-			>
-				<a href={slug} class="group">
-					<article>
+			<li class="">
+				<a href={slug}>
+					<article
+						class="border border-zinc-800 p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/10 hover:border-rose-800"
+					>
 						<h2 class="font-semibold">{title}</h2>
 						<p class="text-zinc-300">{description}</p>
+						<div class="flex gap-2">
+							{#each tags as tag}
+								<p class="text-xs text-zinc-500 uppercase tracking-wider">#{tag}</p>
+							{/each}
+						</div>
 					</article>
-					<div class="flex gap-2">
-						{#each tags as tag}
-							<p class="text-xs text-zinc-500 uppercase tracking-wider">#{tag}</p>
-						{/each}
-					</div>
 				</a>
 			</li>
 		{/each}
--- src/routes/tags/[tag]/+page.svelte
+++ src/routes/tags/[tag]/+page.svelte
@@ -8,21 +8,21 @@
 	<p class="text-3xl font-bold mb-4">
 		Snippets for <span class="uppercase text-zinc-400">#{data.tag}</span>
 	</p>
-	<ul class=" grid grid-cols-2 flex-wrap gap-2">
+	<ul class="grid grid-cols-2 flex-wrap gap-2">
 		{#each data.snippets as { slug, description, title, tags }}
-			<li
-				class="border border-zinc-800 p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/10 hover:border-rose-800"
-			>
-				<a href={slug} class="group">
-					<article>
+			<li class="">
+				<a href={slug}>
+					<article
+						class="border border-zinc-800 p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/10 hover:border-rose-800"
+					>
 						<h2 class="font-semibold">{title}</h2>
 						<p class="text-zinc-300">{description}</p>
+						<div class="flex gap-2">
+							{#each tags as tag}
+								<p class="text-xs text-zinc-500 uppercase tracking-wider">#{tag}</p>
+							{/each}
+						</div>
 					</article>
-					<div class="flex gap-2">
-						{#each tags as tag}
-							<p class="text-xs text-zinc-500 uppercase tracking-wider">#{tag}</p>
-						{/each}
-					</div>
 				</a>
 			</li>
 		{/each}
