ea37752ceb45230911fdd9099a53f44376943728
fabian9799 <fabian9799@protonmail.ch>
2024-04-03 18:58:18 +0200

    style: fix code block width and text wrap

--- src/lib/shiki.ts
+++ src/lib/shiki.ts
@@ -3,6 +3,6 @@ import { getHighlighter } from 'shiki';
 // `getHighlighter` is async, it initializes the internal and
 // loads the themes and languages specified.
 export const highlighter = await getHighlighter({
-	themes: ['night-owl'],
+	themes: ['dracula'],
 	langs: ['svelte']
 });
--- src/markdoc/nodes/Fence.svelte
+++ src/markdoc/nodes/Fence.svelte
@@ -1,24 +1,23 @@
 <script lang="ts">
 	import { highlighter } from '$lib/shiki';
-	import { codeToHtml } from 'shiki';
 
 	export let content: string;
 	export let language: string;
-	export let process: boolean;
 
 	const html = highlighter.codeToHtml(content, {
 		lang: language,
-		theme: 'night-owl',
-		transformers: [
-			{
-				pre(node) {
-					this.addClassToHast(node, 'p-4');
-				}
-			}
-		]
+		theme: 'dracula'
 	});
 </script>
 
-<div class="not-prose border border-zinc-800 rounded-xl overflow-x-auto">
+<div class="not-prose border border-zinc-800 rounded-xl overflow-hidden code-block">
 	{@html html}
 </div>
+
+<style>
+	.code-block :global(pre) {
+		padding: 20px;
+		white-space: pre-wrap;
+		overflow: auto;
+	}
+</style>
