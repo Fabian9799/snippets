4ffa6ac8878eb651b034cc0e63d4308365cb1a24
fabian9799 <fabian9799@protonmail.ch>
2024-04-05 21:38:17 +0200

    style: og image

--- src/routes/og/[slug]/Og.svelte
+++ src/routes/og/[slug]/Og.svelte
@@ -5,7 +5,7 @@
 
 <div class="flex text-3xl h-full bg-zinc-900 p-8">
 	<div
-		class="w-full flex flex-col justify-center items-center text-center border-8 border-rose-600 text-white"
+		class="w-full p-4 flex flex-col justify-center items-center text-center border-8 rounded-[50px] border-rose-600 text-white"
 	>
 		<p class="font-bold" style="font-size: 80px">{title}</p>
 		<p>{description}</p>
