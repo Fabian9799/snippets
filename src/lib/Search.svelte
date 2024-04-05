540246d8b05d7548263316657fcec27a64ba162b
fabian9799 <fabian9799@protonmail.ch>
2024-04-05 14:03:30 +0200

    feat: search

---
+++ src/lib/Search.svelte
@@ -0,0 +1,103 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import { Dialog, Separator, Label } from 'bits-ui';
+	import { fade } from 'svelte/transition';
+	import Article from './Article.svelte';
+
+	type Snippet = {
+		text: string;
+		slug: string;
+		tags: string[];
+		title: string;
+		description: string;
+	};
+
+	const snippets = $page.data.snippets as Snippet[];
+	const formattedSnippets = snippets.map((snippet) => {
+		return {
+			text: `${snippet.title} ${snippet.description} ${snippet.tags.join(' ')}`,
+			slug: snippet.slug,
+			tags: snippet.tags,
+			title: snippet.title,
+			description: snippet.description
+		};
+	});
+
+	let searchTerm = '';
+
+	$: filteredSnippets = formattedSnippets.filter((snippet) => snippet.text.includes(searchTerm));
+</script>
+
+<Dialog.Root>
+	<Dialog.Trigger
+		class="inline-flex size-12 items-center
+    justify-center "
+	>
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width="24"
+			height="24"
+			viewBox="0 0 24 24"
+			fill="none"
+			stroke="currentColor"
+			stroke-width="2"
+			stroke-linecap="round"
+			stroke-linejoin="round"><circle cx="11" cy="11" r="8" /><path d="m21 21-4.3-4.3" /></svg
+		>
+	</Dialog.Trigger>
+	<Dialog.Portal>
+		<Dialog.Overlay
+			transition={fade}
+			transitionConfig={{ duration: 150 }}
+			class="fixed inset-0 z-50 bg-black/80"
+		/>
+		<Dialog.Content
+			class="fixed left-[50%] top-[50%] z-50 w-full max-w-[94%] translate-x-[-50%] translate-y-[-50%] rounded-xl border border-zinc-800 bg-zinc-900 p-5 shadow-xl outline-none sm:max-w-[490px] md:w-full"
+		>
+			<Dialog.Title
+				class="flex w-full items-center justify-center text-2xl mb-4 font-semibold tracking-tight"
+				>Search</Dialog.Title
+			>
+			<Separator.Root class="-mx-5 mb-6 block h-px bg-zinc-800" />
+
+			<div class="flex flex-col items-start gap-4">
+				<div class="relative w-full">
+					<input
+						id="apiKey"
+						class="bg-zinc-800 border border-zinc-700 w-full px-4 py-2 rounded-xl"
+						placeholder="Search..."
+						type="text"
+						autocomplete="off"
+						bind:value={searchTerm}
+					/>
+				</div>
+				<div class="flex flex-col w-full gap-2 max-h-96 overflow-y-auto p-1">
+					{#each filteredSnippets as { description, slug, tags, text, title }}
+						<div class="w-full">
+							<Article {slug} {title} {description} {tags} />
+						</div>
+					{/each}
+				</div>
+			</div>
+
+			<Dialog.Close
+				class="absolute right-5 top-5 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-foreground focus-visible:ring-offset-2 focus-visible:ring-offset-background active:scale-98"
+			>
+				<div>
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						width="24"
+						height="24"
+						viewBox="0 0 24 24"
+						fill="none"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+					>
+					<span class="sr-only">Close</span>
+				</div>
+			</Dialog.Close>
+		</Dialog.Content>
+	</Dialog.Portal>
+</Dialog.Root>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,16 @@
 <script lang="ts">
 	import Article from '$lib/Article.svelte';
+	import Search from '$lib/Search.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
 
 <main class="p-8 max-w-2xl mx-auto">
-	<p class="text-3xl font-bold mb-4">Snippets</p>
+	<div class="flex">
+		<p class="text-3xl flex-1 font-bold mb-4">Snippets</p>
+		<Search />
+	</div>
 	<ul class="grid grid-cols-2 flex-wrap gap-2">
 		{#each data.snippets as { slug, description, title, tags }}
 			<li>
