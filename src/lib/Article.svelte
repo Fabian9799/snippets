89a28fbce7ac556273358a43164f00ebc00f7aad
fabian9799 <fabian9799@protonmail.ch>
2024-04-05 20:20:27 +0200

    content: add apache echarts

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -5,12 +5,12 @@
 	export let tags: string[];
 </script>
 
-<a href="/snippet/{slug}" class="inline-block w-full rounded-md">
+<a href="/snippet/{slug}" class="inline-block w-full rounded-md h-full">
 	<div
-		class="border border-zinc-800 p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/50 hover:ring ring-rose-600"
+		class="border border-zinc-800 h-full flex flex-col p-4 rounded-md shadow hover:bg-gradient-to-r hover:from-rose-900/50 hover:ring ring-rose-600"
 	>
 		<h2 class="font-semibold">{title}</h2>
-		<p class="text-zinc-300">{description}</p>
+		<p class="text-zinc-300 flex-1">{description}</p>
 		<div class="flex gap-2">
 			{#each tags as tag}
 				<p class="text-xs text-zinc-500 uppercase tracking-wider">#{tag}</p>
