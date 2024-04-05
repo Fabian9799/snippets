46e4afe0023426187dd341332f0f8a934f2597d3
fabian9799 <fabian9799@protonmail.ch>
2024-04-05 18:02:51 +0200

    fix: tags page

--- src/routes/tags/[tag]/+page.server.ts
+++ src/routes/tags/[tag]/+page.server.ts
@@ -1,19 +1,7 @@
+import { listSnippets } from '$lib/snippets.js';
+
 export function load({ params }) {
-	const modules = import.meta.glob('../../article/**/*.markdoc', {
-		eager: true
-	});
-
-	const snippets = Object.entries(modules).map(([filepath, module]) => {
-		const { frontmatter } = module;
-		const slug = filepath.split('/+page')[0].replace(/^\./, '');
-
-		return {
-			title: frontmatter.title,
-			description: frontmatter.description,
-			slug,
-			tags: frontmatter.tags
-		};
-	});
+	const snippets = listSnippets();
 
 	const tag = params.tag;
 
