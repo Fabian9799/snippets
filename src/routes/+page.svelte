393599cb721724314bed69463fd75cfda56fe83d
fabian9799 <fabian9799@protonmail.ch>
2024-04-05 21:50:35 +0200

    feat: meta tags for main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,21 @@
 	export let data: PageData;
 </script>
 
-<title>Snippets</title>
+<svelte:head>
+	<title>Snippets</title>
+	<meta property="og:site_name" content="Snippets" />
+	<meta property="og:title" content="Useful Snippets" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="https://snippets.fabian9799.cloud/favicon.png" />
+	<meta name="description" content="A list of some useful snippets" />
+	<meta name="twitter:description" content="A list of some useful snippets" />
+	<meta property="og:description" content="A list of some useful snippets" />
+	<meta property="og:image" content="https://snippets.fabian9799.cloud/favicon.png" />
+	<meta name="twitter:image" content="https://snippets.fabian9799.cloud/favicon.png" />
+	<meta name="twitter:card" content="summary_large_image" />
+	<meta property="og:image:alt" content="A list of some useful snippets" />
+	<meta name="twitter:image:alt" content="A list of some useful snippets" />
+</svelte:head>
 
 <main class="p-8 max-w-2xl mx-auto">
 	<div class="flex">
