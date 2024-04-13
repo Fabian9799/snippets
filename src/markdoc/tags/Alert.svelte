2d4cd3d93b23955042611f3107c576114f9343b9
fabian9799 <fabian9799@protonmail.ch>
2024-04-13 14:04:57 +0200

    fix: add slot to alert

--- src/markdoc/tags/Alert.svelte
+++ src/markdoc/tags/Alert.svelte
@@ -1,8 +1,6 @@
-<script lang="ts">
-	export let text: string;
-</script>
-
-<div class="border-2 my-4 rounded-xl border-rose-600 bg-rose-600/20 p-3 not-prose flex gap-2">
+<div
+	class="border-2 my-4 rounded-xl border-rose-600 bg-rose-600/20 p-3 flex gap-2 text-white tracking-wide"
+>
 	<svg
 		class="h-7"
 		xmlns="http://www.w3.org/2000/svg"
@@ -14,5 +12,7 @@
 		stroke-linejoin="round"
 		><circle cx="12" cy="12" r="10" /><path d="M12 16v-4" /><path d="M12 8h.01" /></svg
 	>
-	<p class="flex-1 text-white tracking-wide">{text}</p>
+	<div class="prose-invert prose prose-rose flex-1">
+		<slot />
+	</div>
 </div>
