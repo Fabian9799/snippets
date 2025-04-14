6cb2e9d8d3db9d697422a199117933729954b939
Fabian <fabian9799@protonmail.ch>
2025-04-14 04:07:02 +0200

    style: better animation

--- src/lib/Logo.svelte
+++ src/lib/Logo.svelte
@@ -1,22 +1,42 @@
 <script lang="ts">
 	import { animate, svg, stagger } from 'animejs';
+	import { createTimeline } from 'animejs';
 
 	let pathElements: HTMLOrSVGElement[] = [];
+	let svgElement: HTMLOrSVGElement;
 
 	function animateLogo() {
-		animate(svg.createDrawable(pathElements), {
-			draw: ['0 0', '0 1'],
-			ease: 'inOutCubic',
-			stroke: ['#ff073a', '#ff6a00', '#ccff00', '#0099ff', '#ffffff'],
-			duration: 2000,
-			delay: stagger(1000)
-		});
+		createTimeline()
+			.add(svg.createDrawable(pathElements), {
+				draw: ['0 1'],
+				duration: 1
+			})
+			.add(svgElement, {
+				height: [40, 0],
+				duration: 250
+			})
+			.add(svg.createDrawable(pathElements), {
+				draw: ['0 0'],
+				stroke: 'transparent'
+			})
+			.add(svgElement, {
+				height: [0, 40],
+				duration: 250
+			})
+			.add(svg.createDrawable(pathElements), {
+				draw: ['0 0', '0 1'],
+				stroke: ['#ff073a', '#ff6a00', '#ccff00', '#0099ff', '#ffffff'],
+				'stroke-linecap': 'square',
+				ease: 'inOutCubic',
+				duration: 500,
+				delay: stagger(1000)
+			});
 	}
 </script>
 
-<button onclick={animateLogo} aria-label="Logo">
+<button onclick={animateLogo} aria-label="Logo" class="w-10" bind:this={svgElement}>
 	<svg
-		class="size-10 fill-none origin-top-left bg-zinc-900"
+		class="size-full fill-none origin-top-left"
 		viewBox=".25 .25 4 4"
 		xmlns="http://www.w3.org/2000/svg"
 	>
