3304f2cdfdd61cd4800d83f3ad41afbd48903ade
Fabian <fabian9799@protonmail.ch>
2026-03-26 20:14:01 +0100

    upgrade packages

---
+++ src/routes/+error.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import { page } from '$app/state';
+</script>
+
+<div class="w-full min-h-screen flex items-center justify-center">
+    <h1 class="text-4xl font-bold">{page.status}: {page.error?.message}</h1>
+</div>
